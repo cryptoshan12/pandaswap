11e8462c5ec6980e1c0132f8f05e084cee9043ab
cryptoshan12 <105999288+cryptoshan12@users.noreply.github.com>
2024-01-09 02:39:38 +0500

    Update endpoints.ts

--- apps/web/src/config/constants/endpoints.ts
+++ apps/web/src/config/constants/endpoints.ts
@@ -17,8 +17,8 @@ export const GRAPH_API_POTTERY = 'https://api.thegraph.com/subgraphs/name/pancak
  */
 export const GRAPH_API_PREDICTION_V1 = 'https://api.thegraph.com/subgraphs/name/pancakeswap/prediction'
 
-export const INFO_CLIENT = 'https://proxy-worker.pancake-swap.workers.dev/bsc-exchange'
-
+export const INFO_CLIENT = 'https://open-platform.nodereal.io/bbed521f0fa842afb42a9bed223410f1/pancakeswap-free/graphql'
+//**https://proxy-worker.pancake-swap.workers.dev/bsc-exchange**/
 export const INFO_CLIENT_ETH = 'https://api.thegraph.com/subgraphs/name/pancakeswap/exhange-eth'
 export const BLOCKS_CLIENT = 'https://api.thegraph.com/subgraphs/name/pancakeswap/blocks'
 export const BLOCKS_CLIENT_ETH = 'https://api.thegraph.com/subgraphs/name/blocklytics/ethereum-blocks'
